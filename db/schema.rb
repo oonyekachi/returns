115f43b9b266dab6b58c902ea5779d31915aa076
Damilola Odujoko <damilola.o@ng.lopworks.com>
2017-01-03 08:07:35 +0100

    Create help model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214143154) do
+ActiveRecord::Schema.define(version: 20161229151522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,13 @@ ActiveRecord::Schema.define(version: 20161214143154) do
     t.date     "filer_date"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+  end
+
+  create_table "helps", force: :cascade do |t|
+    t.text     "description"
+    t.string   "description_type"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   create_table "line_items", force: :cascade do |t|
